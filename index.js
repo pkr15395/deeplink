b0bc56d3bee3ccd447c00a9173b8eb34f1fb4916
Praveen Rai <PraveenR@coreworx.com>
2021-09-09 17:01:14 +0530

    a

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .use('/.well-known', express.static('.well-known'))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/.well-known/assetlinks.json', (req, res) => {
+	  res.writeHead(200, { 'Content-Type': contentType });
+      res.end("Test", 'utf-8');
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
