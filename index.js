ba8b16d44ff2405ba314f46034fb575cf606b2f3
Praveen Rai <PraveenR@coreworx.com>
2021-09-09 16:44:22 +0530

    a

--- index.js
+++ index.js
@@ -4,9 +4,7 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .use('/.well-known/assetlinks.json', express.static(__dirname + '.well-known/assetlinks.json'))
-  .use(express.static(path.join(__dirname, '.well-known')))
-  .set('.well-known/assetlinks.json', path.join(__dirname + '.well-known/assetlinks.json'))
+  .use('/.well-known', express.static('.well-known'))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
